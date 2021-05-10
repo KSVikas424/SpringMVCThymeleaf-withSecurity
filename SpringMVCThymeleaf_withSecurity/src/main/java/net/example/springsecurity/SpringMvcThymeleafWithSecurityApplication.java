2f7c42e9699850da7cfbdab2de7521c5b213074f
ksvikas424 <srinivasvikask@gmail.com>
2021-05-10 15:25:36 +0530

    Created a webpage Welcome and Hello, There is no security for Welcome page and there is Security for Hello page

---
+++ SpringMVCThymeleaf_withSecurity/src/main/java/net/example/springsecurity/SpringMvcThymeleafWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package net.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymeleafWithSecurityApplication.class, args);
+	}
+
+}
